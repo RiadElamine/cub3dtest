32293d35785a3574810f46531951a54d2db3b7ec
Riad <RiadElamineDevInfo@gmail.com>
2024-12-22 21:15:23 -0500

    fix parsing

--- parsing_texture.c
+++ parsing_texture.c
@@ -38,26 +38,51 @@ int is_texture_valid(t_map *map)
 	return (1);
 }
 
+char *get_value(char *line, char **texture, int *status)
+{
+	int argc;
+
+	argc = ft_count(texture);
+	if (argc >= 3)
+	{
+		char *tmp = ft_strnstr(line, texture[0], ft_strlen(line));
+		if (!tmp)
+			return (ft_free(texture), *status = 0, NULL);
+		tmp = ft_substr(tmp, ft_strlen(texture[0]), ft_strlen(tmp) - 2);
+		tmp = ft_strtrim(tmp, " \t\v\f\r\n");
+		if (!tmp)
+			return (ft_free(texture), *status = 0, NULL);
+		return (*status = 1, tmp);
+	}
+	return (*status = 1, texture[1]);
+}
+
 int parsing_texture(char *line, t_map *map, int counter)
 {
 	char **texture;
+	int status;
 
+	status = 1;
 	if (counter == 6)
 		return (2);
 	texture = my_split(line, "\t\v\f\r\n ");
 	if (!texture)
 		return (0);
-	if (ft_count(texture) != 2)
+	if (ft_count(texture) < 2)
 		return (ft_free(texture), 0);
 	else if (!ft_strcmp(texture[0], "NO"))
-		map->no = texture[1];
+		map->no = get_value(line, texture, &status);
 	else if (!ft_strcmp(texture[0], "SO"))
-		map->so = texture[1];
+		map->so = get_value(line, texture, &status);
 	else if (!ft_strcmp(texture[0], "WE"))
-		map->we = texture[1];
+		map->we = get_value(line, texture, &status);
 	else if (!ft_strcmp(texture[0], "EA"))
-		map->ea = texture[1];
+		map->ea = get_value(line, texture, &status);
 	else
 		return (ft_free(texture), 0);
+	if (!status)
+		return (ft_free(texture), 0);
+	if (map->no && map->so && map->we && map->ea)
+		return (free(texture[0]), free(texture), is_texture_valid(map));
 	return (free(texture[0]), free(texture), 1);
 }
